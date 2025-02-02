3b8a1839d2d9f7dc7e9dad2389d0e0a76df31fb6
Japnoor27 <japnoor1106.becse24@chitkara.edu.in>
2025-02-02 12:32:53 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -5,7 +5,7 @@ scanf("%c",&ch);
 if(ch>='0' && ch<= '9'){
     printf("Digit");}
     else if ((ch>='A'&& ch<='Z')|| (ch>='a' && ch<='z')){
-        if (ch=='A' ||ch=='E'|| ch=='I' ||ch=='O'||ch=='U'ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+        if (ch =='A' ||ch =='E'|| ch =='I' ||ch =='O'||ch =='U'||ch =='a'||ch =='e'||ch =='i'||ch =='o'||ch =='u'){
             printf("Vowel");
         }
         else{
