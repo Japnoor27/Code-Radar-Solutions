4813163732c9e1f2a76f2d5e6019589efd1fccc9
Japnoor27 <japnoorkd@gmail.com>
2025-03-30 00:15:17 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -1,32 +1,45 @@
 #include <stdio.h>
-int main(){
-    int N,a[100]={},i,j,freq[100]={},count=1,flag=0;
-    scanf("%d",&N);
-    for(i=0;i<N;i++){
-        scanf("%d",&a[i]);
-      }
-      for(i=0;i<N-1;i++){
-         flag=1;
-        for(j=0;j<N-1-i;j++){
-           
-            if(a[j]>a[j+1]){
-                flag=1;
-               int temp =a[j];
-               a[j]=a[j+1];
-               a[j+1]=temp;
-            }
-            else if(a[j]==a[j+1]){
-                flag=0;
+
+int main() {
+    int N, a[100], i, j, flag = 0;
+
+    // Input the size of the array
+    scanf("%d", &N);
+
+    // Input elements of the array
+    for (i = 0; i < N; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Handle edge case where array size is less than 2
+    if (N < 2) {
+        printf("-1\n");  // Second smallest doesn't exist
+        return 0;
+    }
+
+    // Sort the array using Bubble Sort
+    for (i = 0; i < N - 1; i++) {
+        for (j = 0; j < N - 1 - i; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
         }
-      }
-    
-      if(N==1 || flag==0){
-        printf("-1");
-      }
-      else{
-        printf("%d",a[1]);
-      }
-     return 0;
-      
-      }
+    }
+
+    // Find the second smallest element
+    int first = a[0];
+    int second = -1;  // Default to -1 if no second smallest exists
+
+    for (i = 1; i < N; i++) {
+        if (a[i] > first) {
+            second = a[i];
+            break;
+        }
+    }
+
+    printf("%d\n", second);
+
+    return 0;
+}
