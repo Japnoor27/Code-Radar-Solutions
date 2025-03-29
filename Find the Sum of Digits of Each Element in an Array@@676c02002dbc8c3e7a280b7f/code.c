3e2338530e50bcff7094b370246e33f51f6b39b6
Japnoor27 <japnoorkd@gmail.com>
2025-03-30 02:19:01 +0530

    Solution to Find the Sum of Digits of Each Element in an Array

--- Find the Sum of Digits of Each Element in an Array@@676c02002dbc8c3e7a280b7f/code.c
+++ Find the Sum of Digits of Each Element in an Array@@676c02002dbc8c3e7a280b7f/code.c
@@ -13,10 +13,19 @@ int main() {
    
     for (i = 0; i < N; i++) {
         sum = 0; 
+        if(a[i]>0){
         while (a[i] != 0) {
             n = a[i] % 10;    
             sum =sum+ n;         
             a[i] =a[i]/ 10;       
+        }}
+        else{
+             while (a[i] != 0) {
+                num=-(a[i]);
+            n = num % 10;    
+            sum =sum+ n;         
+            num =num/ 10;       
+        }
         }
         printf("%d ", sum);  
     }
