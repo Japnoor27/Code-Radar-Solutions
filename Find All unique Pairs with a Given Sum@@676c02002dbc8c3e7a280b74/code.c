0690913404727168a2182a8a3d66f638693262bd
Japnoor27 <japnoorkd@gmail.com>
2025-03-30 01:28:26 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
-int main(){
-    int i,n,a[100]={},pair=0,j,t;
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-        scanf("%d",&a[i]);
+
+int main() {
+    int i, n, a[100] = {}, pair = 0, j, t;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
-    scanf("%d",&t);
-    for(i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
-            if(a[i]+a[j]==t){
-                 printf("%d %d\n",a[i],a[j]);
+    scanf("%d", &t);
+
+    // Sort the array (simple bubble sort, replace with efficient sort if needed)
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
         }
     }
-   
+
+    // Find unique pairs
+    for (i = 0; i < n; i++) {
+        if (i > 0 && a[i] == a[i - 1]) continue; // Skip duplicates
+        for (j = i + 1; j < n; j++) {
+            if (j > i + 1 && a[j] == a[j - 1]) continue; // Skip duplicates
+            if (a[i] + a[j] == t) {
+                printf("%d %d\n", a[i], a[j]);
+            }
+        }
+    }
 }
