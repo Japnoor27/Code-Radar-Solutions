d47217d46c3b387cc84b9366f4af4bf22817e868
Japnoor27 <japnoorkd@gmail.com>
2025-03-31 17:06:26 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
-int selectionSort(int arr[],int n){
-    int i,j,min,minidx;
-    for(int i=0;i<n;i++){
-     min=arr[i];
-     minidx=i;
-    }
-    for(int j=i;j<n;j++){
-        if(min>arr[j]){
-            min=arr[j];
-            minidx=j;
+
+// Selection Sort Function
+void selectionSort(int arr[], int n) {
+    int i, j, minidx;
+    for (i = 0; i < n - 1; i++) {
+        minidx = i; // Assume the current element is the smallest
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minidx]) {
+                minidx = j; // Update index of the smallest element
+            }
         }
-    }
-    int temp=arr[minidx];
-    arr[minidx]=arr[i];
-    arr[i]=temp;
-}
-int printArray(int arr[], int n){
-    for(int i=0;i<n;i++){
-        printf("%d ",arr[i]);
+        // Swap the found smallest element with the first element
+        int temp = arr[minidx];
+        arr[minidx] = arr[i];
+        arr[i] = temp;
     }
 }
+
+// Function to print array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
